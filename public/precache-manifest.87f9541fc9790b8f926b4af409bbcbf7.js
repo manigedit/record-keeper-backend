b99a2d8bf8dce42f146bed43bcc12000125e8879
manigedit <1manish22@gmail.com>
2020-09-14 08:57:59 +0530

    build: adds frontend static

---
+++ public/precache-manifest.87f9541fc9790b8f926b4af409bbcbf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb9811883c9397d594289c358de72b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c9cf646192a20732383",
+    "url": "/static/css/main.07f1f040.chunk.css"
+  },
+  {
+    "revision": "b60c08e0663f6a52e425",
+    "url": "/static/js/2.fab80444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fab80444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9cf646192a20732383",
+    "url": "/static/js/main.581dfd90.chunk.js"
+  },
+  {
+    "revision": "f24d55873db7e1ef0ffa",
+    "url": "/static/js/runtime-main.9d42f928.js"
+  }
+]);
